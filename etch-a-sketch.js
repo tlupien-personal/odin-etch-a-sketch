05d8d5be320ba7387cf8c0e458936e0691d2875f
Tim Lupien <timlupien@gmail.com>
2026-03-16 19:31:56 -0500

    Add reset button
    Adds the reset button with accompanying changes to the CSS to have
    the grid behave properly under resets to different sizes and the
    addition of the appropriate event handler for resets.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -19,4 +19,34 @@ const makeSquares = function (size) {
   }
 };
 
+const resetGrid = function () {
+  const oldGridArea = document.querySelector("#grid-area");
+  oldGridArea.remove();
+  const newGridArea = document.createElement("div");
+  newGridArea.id = "grid-area";
+  const body = document.querySelector("body");
+  body.appendChild(newGridArea) 
+}
+
+const promptForSize = function() {
+  const promptText = "Enter the new number of squares per side:"
+  let size = prompt(promptText)
+  while (!Number.isInteger(size) && size >= 100) {
+    if (size === null || size === "") {
+      return null
+    }
+    size = prompt(promptText)
+  }
+  return size;
+}
+
+const resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", (e) => {
+  const size = promptForSize();
+  if (size) {
+    resetGrid();
+    makeSquares(size);
+  }
+})
+
 makeSquares(16);
